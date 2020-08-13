8a60321cc36e97482b44019ae5b93d446752dd7f
Zain Ul Abaidin <zainulabedin5566@gmail.com>
2020-08-14 01:11:04 +0500

    zain commit

--- pages.js
+++ pages.js
@@ -1,6 +1,6 @@
 const ghpages = require("gh-pages");
 const pathname = `${__dirname}/build`;
-const repoURL = "https://github.com/hashirshoaeb/hashirshoaeb.github.io.git";
+const repoURL = "https://github.com/Zainulabaidin5566/Zainulabaidin5566.github.io.git";
 
 ghpages.publish(
   pathname,
